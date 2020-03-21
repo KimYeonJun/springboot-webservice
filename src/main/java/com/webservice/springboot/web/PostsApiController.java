dce6074f7387b584a87b1543ed49cb7180a353ae
KimYeonJun <duswns1783@naver.com>
2020-03-21 22:07:16 +0900

    수정/조회 Controller 추가

--- src/main/java/com/webservice/springboot/web/PostsApiController.java
+++ src/main/java/com/webservice/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.webservice.springboot.web;
 
 import com.webservice.springboot.service.PostsService;
+import com.webservice.springboot.web.dto.PostsResponseDto;
 import com.webservice.springboot.web.dto.PostsSaveRequestDto;
+import com.webservice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor  //final 키워드 필드에 대한 생성자를 만든다.
 @RestController //컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
@@ -15,6 +15,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 
 }
