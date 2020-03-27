0ea60d745e916984cba51d329d9457c0943b7af7
KimYeonJun <duswns1783@naver.com>
2020-03-27 16:57:01 +0900

    스프링 시큐리티 관련 설정 등록

---
+++ src/main/java/com/webservice/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.webservice.springboot.config.auth;
+
+import com.webservice.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+/*
+이 클래스에서 스프링 시큐리티 관련 왠만한 설정들이 다 들어간다고 해도 무방하다!
+ */
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+
+    }
+}
